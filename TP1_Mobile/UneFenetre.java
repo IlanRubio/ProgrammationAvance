ba2677abeec9ef5a74ee7f2330cdf41a48b0a747
IRubio <ilanrubio4@gmail.com>
2024-09-27 12:24:55 +0200

    Fin de seance 27/09

--- TP1_Mobile/UneFenetre.java
+++ TP1_Mobile/UneFenetre.java
@@ -1,17 +1,26 @@
 import java.awt.*;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-class UneFenetre extends JFrame 
-{
+class UneFenetre extends JFrame {
     UnMobile sonMobile;
-    private final int LARG=400, HAUT=250;
-    
-    public UneFenetre()
-    {
-	// TODO 
-	// ajouter sonMobile a la fenetre
-	// creer une thread laThread avec sonMobile
-	// afficher la fenetre
-	// lancer laThread 
+    JButton sonButton;
+    private final int LARG = 400, HAUT = 250;
+
+    public UneFenetre() {
+        // TODO
+        super("le Mobile");
+        Container leContainer = getContentPane();
+        sonMobile = new UnMobile(LARG, HAUT);
+        sonButton = new JButton("Start/Stop");
+
+        leContainer.add(sonButton);
+        leContainer.add(sonMobile);
+        // creer une thread laThread avec sonMobile
+        Thread laTache = new Thread(sonMobile);
+        laTache.start();
+        setSize(LARG, HAUT);
+        setVisible(true);
+        // lancer laThread
     }
 }
---
+++ TP1_Mobile/rapport.md
@@ -0,0 +1,4 @@
+Ecriture de la fonction uneFenetre 
+--> le mobile va de gauche à droite et s'arrête à droite
+
+Modification de run de la classe UnMobile pour qu'il aille de droite à gauche
