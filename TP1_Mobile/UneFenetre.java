90d2b321ed83c1941038e26451df9f7c2d81c586
IRubio <ilanrubio4@gmail.com>
2024-10-25 10:12:48 +0200

    TP1 - seance 25/10 Ajout d'un troisième mobile

--- TP1_Mobile/UneFenetre.java
+++ TP1_Mobile/UneFenetre.java
@@ -5,31 +5,32 @@ import javax.swing.*;
 class UneFenetre extends JFrame {
     UnMobile sonMobile;
     UnMobile sonMobile1;
-    JButton sonButton;
-    JButton sonButton1;
+    UnMobile sonMobile2;
     private final int LARG = 400, HAUT = 250;
 
     public UneFenetre() {
         // TODO
         super("le Mobile");
         Container leContainer = getContentPane();
-        leContainer.setLayout(new GridLayout(2, 1));
+        leContainer.setLayout(new GridLayout(3, 1));
         sonMobile = new UnMobile(LARG, HAUT);
-        //sonButton = new JButton("Start/Stop");
         leContainer.add(sonMobile);
-        //leContainer.add(sonButton);
+
         // creer une thread laThread avec sonMobile
         Thread laTache = new Thread(sonMobile);
         laTache.start();
 
         sonMobile1 = new UnMobile(LARG, HAUT);
-        // sonButton1 = new JButton("Start/Stop");
-        // leContainer.add(sonButton1);
         leContainer.add(sonMobile1);
         Thread laTache1 = new Thread(sonMobile1);
         laTache1.start();
 
-        setSize(LARG, HAUT);
+        sonMobile2 = new UnMobile(LARG, HAUT);
+        leContainer.add(sonMobile2);
+        Thread laTache2 = new Thread(sonMobile2);
+        laTache2.start();
+
+        setSize(3000, 2500);
         setVisible(true);
         // lancer laThread
     }
