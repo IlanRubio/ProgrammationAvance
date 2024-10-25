60ff1d8e9d8bec64aaeba3206d1bcfa5f449687d
IRubio <ilanrubio4@gmail.com>
2024-10-25 10:10:18 +0200

    TP2 - seance 25/10 Modification pour fonctionner avec semaphore

--- TP2_Affichage/Affichage.java
+++ TP2_Affichage/Affichage.java
@@ -10,20 +10,20 @@ import java.lang.String;
 class Exclusion{};
 public class Affichage extends Thread{
 	String texte;
-	semaphoreBinaire sem = new semaphoreBinaire(1);
+	static semaphoreBinaire sem = new semaphoreBinaire(1);
         
 	//static Exclusion exclusionMutuelle = new Exclusion();
 
 	public Affichage (String txt){texte=txt;}
 	
 	public void run(){
-		//sem.syncWait();
-	    synchronized (System.out) { //section critique
+		sem.syncWait();
+	    //synchronized (System.out) { //section critique
 	    for (int i=0; i<texte.length(); i++){
 		    System.out.print(texte.charAt(i));
 		    try {sleep(400);} catch(InterruptedException e){};
 		}
-		//sem.syncSignal();
-	    }
+		sem.syncSignal();
+	    //}
 	}
 }
