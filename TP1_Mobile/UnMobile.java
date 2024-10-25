e8378059e4578b860f2ae4e5737d158fe7880350
IRubio <ilanrubio4@gmail.com>
2024-10-25 10:56:51 +0200

    TP1 - seance 25/10 Création de la classe sémaphore global, ajout des semaphores pour faire des pauses aux mobiles

--- TP1_Mobile/UnMobile.java
+++ TP1_Mobile/UnMobile.java
@@ -1,9 +1,12 @@
 import java.awt.*;
+import java.util.concurrent.Semaphore;
 import javax.swing.*;
 
 class UnMobile extends JPanel implements Runnable {
     int saLargeur, saHauteur, sonDebDessin;
-    final int sonPas = 10, sonTemps = 50, sonCote = 40;
+    final int sonPas = 10,  sonCote = 40;
+    int sonTemps = (int) (Math.random() * ((40 - 5) + 1)) + 5;
+    static semaphoreGlobal sem = new semaphoreGlobal(1);
 
     UnMobile(int telleLargeur, int telleHauteur) {
         super();
@@ -13,20 +16,70 @@ class UnMobile extends JPanel implements Runnable {
     }
 
     public void run() {
-        for (sonDebDessin = 0; sonDebDessin < saLargeur - sonPas; sonDebDessin += sonPas) {
-            repaint();
-            try {
-                Thread.sleep(sonTemps);
-            } catch (InterruptedException telleExcp) {
-                telleExcp.printStackTrace();
+        while (true) { //pour le faire tourner en permanence
+            // du debut au premier tiers
+            for (sonDebDessin = 0; sonDebDessin < saLargeur - sonPas; sonDebDessin += sonPas) {
+                repaint();
+                try {
+                    Thread.sleep(sonTemps);
+                } catch (InterruptedException telleExcp) {
+                    telleExcp.printStackTrace();
+                }
             }
-        }
-        for (sonDebDessin = saLargeur - sonPas; sonDebDessin > 0; sonDebDessin -= sonPas) {
-            repaint();
-            try {
-                Thread.sleep(sonTemps);
-            } catch (InterruptedException telleExcp) {
-                telleExcp.printStackTrace();
+
+
+            // premier tiers au deuxieme
+            sem.syncWait();
+            for (sonDebDessin = saLargeur; sonDebDessin < 2 * saLargeur - sonPas; sonDebDessin += sonPas) {
+                repaint();
+                try {
+                    Thread.sleep(sonTemps);
+                } catch (InterruptedException telleExcp) {
+                    telleExcp.printStackTrace();
+                }
+            }
+            sem.syncSignal();
+
+            // deuxieme tiers à la fin
+            for (sonDebDessin = 2 * saLargeur; sonDebDessin < 3 * saLargeur - sonPas; sonDebDessin += sonPas) {
+                repaint();
+                try {
+                    Thread.sleep(sonTemps);
+                } catch (InterruptedException telleExcp) {
+                    telleExcp.printStackTrace();
+                }
+            }
+
+            // fin au deuxieme tiers
+            for (sonDebDessin = 3 * saLargeur - sonPas; sonDebDessin > 2 * saLargeur; sonDebDessin -= sonPas) {
+                repaint();
+                try {
+                    Thread.sleep(sonTemps);
+                } catch (InterruptedException telleExcp) {
+                    telleExcp.printStackTrace();
+                }
+            }
+
+            //deuxieme tiers au premier
+            sem.syncWait();
+            for (sonDebDessin = 2 * saLargeur - sonPas; sonDebDessin > saLargeur; sonDebDessin -= sonPas) {
+                repaint();
+                try {
+                    Thread.sleep(sonTemps);
+                } catch (InterruptedException telleExcp) {
+                    telleExcp.printStackTrace();
+                }
+            }
+            sem.syncSignal();
+
+            //premier tiers au debut
+            for (sonDebDessin = saLargeur - sonPas; sonDebDessin > 0; sonDebDessin -= sonPas) {
+                repaint();
+                try {
+                    Thread.sleep(sonTemps);
+                } catch (InterruptedException telleExcp) {
+                    telleExcp.printStackTrace();
+                }
             }
         }
     }
---
+++ TP1_Mobile/semaphoreGlobal.java
@@ -0,0 +1,5 @@
+public final class semaphoreGlobal extends semaphore{
+    public semaphoreGlobal(int valeurInitiale){
+        super(valeurInitiale);
+    }
+}
